1812b536495d1f25f3597bb0abdf7d8f3d6d8a7b
Sergio Cabral <git@sergiocabral.com>
2020-11-20 21:37:26 -0300

    Renomeado objetos do banco de dados para inglês.

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <html>
 	<head>
 		<title>
-			Redirecionando...
+			Redirecting...
 		</title>
 		<style>
 			html {
@@ -10,56 +10,68 @@
 				background-image: url(loading.gif);
 				background-repeat: no-repeat;
 				background-position: center;
+				background-size: cover;
 			}
 		</style>
 	</head>
-<body>
-	<?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+	<body>
+		<?php
+		ini_set('display_errors', 1);
+		ini_set('display_startup_errors', 1);
+		error_reporting(E_ALL);
 
-function error_handler()
-{
-	    if (error_get_last()) {
-		            header("location:https://www.jw.org/");
-			        }
-}
-set_error_handler("error_handler");
-register_shutdown_function("error_handler");
+		function error_handler()
+		{
+		    $error = error_get_last();
+			if ($error) {
+				header("location:https://sergiocabral.com/");
+			}
+		}
 
-$de = $_SERVER['QUERY_STRING'];
+		set_error_handler("error_handler");
+		register_shutdown_function("error_handler");
 
-$mysqli = new mysqli('server', 'username', 'password', 'database');
+		$works = false;
 
-$stmt = $mysqli->prepare("SELECT id, para, contador, ultimo_acesso FROM de_para WHERE de = ?");
-$stmt->bind_param('s', $de);
-$stmt->execute();
-$stmt->bind_result($de_para_id, $para, $contador, $ultimo_acesso);
-$fetch = $stmt->fetch();
-$stmt->close();
+		$shortcut = $_SERVER['QUERY_STRING'];
 
-if ($fetch && trim($para)) {
-	    $stmt = $mysqli->prepare("INSERT INTO acessos (de_para_id, HTTP_REFERER, REMOTE_HOST, REMOTE_ADDR, HTTP_USER_AGENT) VALUES (?, ?, ?, ?, ?)");
-	        $stmt->bind_param('dssss', $de_para_id, $http_referer, $remote_host, $remote_addr, $http_user_agent);
-	        $http_referer = isset($_SERVER['HTTP_REFERER']) ? $_SERVER['HTTP_REFERER'] : null;
-		    $remote_host = $_SERVER['REMOTE_HOST'];
-		    $remote_addr = $_SERVER['REMOTE_ADDR'];
-		        $http_user_agent = $_SERVER['HTTP_USER_AGENT'];
-		        $stmt->execute();
-			    $stmt->close();
+		$mysqlServer = "server";
+		$mysqlUsername = "username";
+		$mysqlPassword = "password";
+		$mysqlDatabase = "database";
 
-			    $stmt = $mysqli->prepare("UPDATE de_para SET contador = COALESCE(contador, 0) + 1, ultimo_acesso = NOW() WHERE de = ?");
-			        $stmt->bind_param('s', $de);
-			        $stmt->execute();
-				    $stmt->close();
+		$mysqli = new mysqli($mysqlServer, $mysqlUsername, $mysqlPassword, $mysqlDatabase);
 
-				    header("location:" . $para);
-} else {
-	    throw new Exception(-1);
-}
+		$stmt = $mysqli->prepare("SELECT `id`, `destination`, `counter`, `lastAccess` FROM `shortener` WHERE `shortcut` = ?");
+		$stmt->bind_param('s', $shortcut);
+		$stmt->execute();
+		$stmt->bind_result($shortenerId, $destination, $counter, $lastAccess);
+		$fetch = $stmt->fetch();
+		$stmt->close();
 
-$mysqli->close();
-?>
-</body>
+		if ($fetch && trim($destination)) {
+			$stmt = $mysqli->prepare("INSERT INTO `access` (`shortenerId`, `httpReferer`, `remoteHost`, `remoteAddr`, `httpUserAgent`) VALUES (?, ?, ?, ?, ?)");
+			$stmt->bind_param('dssss', $shortenerId, $httpReferer, $remoteHost, $remoteAddr, $httpUserAgent);
+			$httpReferer = isset($_SERVER['HTTP_REFERER']) ? $_SERVER['HTTP_REFERER'] : null;
+			$remoteHost = $_SERVER['REMOTE_HOST'];
+			$remoteAddr = $_SERVER['REMOTE_ADDR'];
+			$httpUserAgent = $_SERVER['HTTP_USER_AGENT'];
+			$stmt->execute();
+			$stmt->close();
+
+			$stmt = $mysqli->prepare("UPDATE `shortener` SET `counter` = COALESCE(`counter`, 0) + 1, `lastAccess` = NOW() WHERE `shortcut` = ?");
+			$stmt->bind_param('s', $shortcut);
+			$stmt->execute();
+			$stmt->close();
+
+			header("location:" . $destination);
+			$works = true;
+		}
+		
+		$mysqli->close();
+
+		if (!$works) throw new Exception(-1);
+
+		?>
+	</body>
 </html>
